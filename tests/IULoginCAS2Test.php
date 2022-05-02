1149f2b9f596a182023ee8e89f28707d9f041e3d
Qiwen Zhu <qiwzhu@iu.edu>
2022-05-02 10:34:42 -0400

    prepare server for tests

--- tests/IULoginCAS2Test.php
+++ tests/IULoginCAS2Test.php
@@ -3,15 +3,48 @@
 namespace Edu\IU\VPCM\IULoginCAS;
 require __DIR__ . '/../src/IULoginCAS2.php';
 
+use phpDocumentor\Reflection\Types\This;
 use PHPUnit\Framework\TestCase;
 
 class IULoginCAS2Test extends TestCase{
+    public const SERVER_DIR = __DIR__ . DIRECTORY_SEPARATOR . 'server';
+    public const PID_FILE = self::SERVER_DIR . DIRECTORY_SEPARATOR . '.pids';
 
+    /**
+     * set up local server for tests
+     */
     public static function setUpBeforeClass(): void
     {
-        //TODO: initiate local test server
-
+        chdir(self::SERVER_DIR);
+        shell_exec("php -S localhost:12345 > /dev/null 2>&1 & echo $! >> " . self::PID_FILE );
     }
+
+    /**
+     * destroy local server after test
+     */
+    public static function tearDownAfterClass(): void
+    {
+        $pids = file_exists(self::PID_FILE) ? file(self::PID_FILE) : false;
+        self::localServer($pids, $killOrder);
+    }
+
+
+    /**
+     * @param $pids
+     * kill server if still active, otherwise just ls
+     */
+    public static function localServer($pids)
+    {
+        if(!$pids){
+            return;
+        }
+
+        foreach ($pids as $pid){
+            $cmd = posix_getpgid($pid) ? 'kill -9 ' . $pid : 'ls';
+            shell_exec($cmd);
+        }
+    }
+
 
     public function setUp(): void
     {
