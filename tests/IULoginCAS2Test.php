d16aaa6f9b9a9dcde24a2dc633f33b3d2bf023e6
Qiwen Zhu <qiwzhu@iu.edu>
2022-05-02 10:09:56 -0400

    add tests

---
+++ tests/IULoginCAS2Test.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Edu\IU\VPCM\IULoginCAS;
+require __DIR__ . '/../src/IULoginCAS2.php';
+
+use PHPUnit\Framework\TestCase;
+
+class IULoginCAS2Test extends TestCase{
+
+    public static function setUpBeforeClass(): void
+    {
+        //TODO: initiate local test server
+
+    }
+
+    public function setUp(): void
+    {
+
+    }
+
+    public function testGetCasTicket()
+    {
+        
+    }
+
+    public function testGetServiceUrl()
+    {
+        
+    }
+
+    public function testGetCasUrlBase()
+    {
+        
+    }
+
+    public function testGetLoginUrl()
+    {
+        
+    }
+
+    public function testGetValidateUrl()
+    {
+        
+    }
+
+    public function testIsAuthenticated()
+    {
+        
+    }
+}
+
